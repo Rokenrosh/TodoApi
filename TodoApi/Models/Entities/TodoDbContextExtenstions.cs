6a5417a2f716fae27b85034ed78c8797f17db976
Kirill <rokenroshr@gmail.com>
2019-06-21 20:15:19 +0300

    added extensions for DbContext

---
+++ TodoApi/Models/Entities/TodoDbContextExtenstions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models.Entities
+{
+    public static class TodoDbContextExtenstions
+    {
+        public static IQueryable<Task> GetTasks(this TodoDbContext dbContext)
+        {
+            var query = dbContext.Tasks.AsQueryable();
+            return query;
+        }
+
+        public static async Task<Task> GetTaskAsync(this TodoDbContext dbContext, Task entity)
+            => await dbContext.Tasks.FirstOrDefaultAsync(item => item.TaskId == entity.TaskId);
+
+        public static async Task<Task> GetTaskByNameAsync(this TodoDbContext dbContext, Task entity)
+            => await dbContext.Tasks.FirstOrDefaultAsync(item => item.Name == entity.Name);
+    }
+}
