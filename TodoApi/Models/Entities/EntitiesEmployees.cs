465ffdc6a280e4bae99d2709c690cd55f8509e89
Филипп Хамицевич <spritefok@gmail.com>
2019-06-21 18:04:22 +0300

    Added TodoDbContext

--- TodoApi/Models/Entities/EntitiesEmployees.cs
+++ TodoApi/Models/Entities/EntitiesEmployees.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ namespace TodoApi.Models.Entities
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Position { get; set; }
@@ -18,6 +21,22 @@ namespace TodoApi.Models.Entities
         public Employee()
         {
 
+        }
+    }
+
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(p => p.EmployeeId);
+
+            builder.Property(p => p.Name).HasColumnType("nvarchar(100)").IsRequired();
+            builder.Property(p => p.Surname).HasColumnType("nvarchar(100)").IsRequired();
+            builder.Property(p => p.Position).HasColumnType("nvarchar(100)").IsRequired();
+            builder.Property(p => p.RoleId).HasColumnType("int").IsRequired();
+            builder.Property(p => p.Email).HasColumnType("nvarchar(255)").IsRequired();
+            builder.Property(p => p.Password).HasColumnType("nvarchar(100)").IsRequired();
+            builder.Property(p => p.Nickname).HasColumnType("nvarchar(30)").IsRequired();
         }
     }
 
---
+++ TodoApi/Models/Entities/TodoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models.Entities
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TasksConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+            : base(options) { }
+    }
+}
