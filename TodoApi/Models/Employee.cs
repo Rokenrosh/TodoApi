b931f4e8a506843719040821e6fd073c4fd83d32
Филипп Хамицевич <spritefok@gmail.com>
2019-06-21 16:14:25 +0300

    Added employee and role models

---
+++ TodoApi/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public Employee()
+        {
+
+        }
+    }
+}
